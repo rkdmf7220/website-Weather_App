b40085ea777fc95001823c4b02e346a8d00aa056
LSM <lsm4551@on.mjc.ac.kr>
2022-05-13 21:00:10 +0900

    Dashboard, Setting 페이지 생성

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{this.$store.mediumLandForecast}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+  mounted() {
+    this.$store.dispatch('updateMediumLandForecast', {regId: '11B00000', tmFc: '202205061800'})
+    this.$store.dispatch('updateVillageForecast')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
