633c523969d0ed1b0ff805035f2ce8473910f6ee
LSM <lsm4551@on.mjc.ac.kr>
2022-05-14 18:48:17 +0900

    스타일 수정

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,8 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap');
+@import "assets/scss/reset.scss";
 #nav {
   padding: 30px;
 }
