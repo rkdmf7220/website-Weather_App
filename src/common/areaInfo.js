79c9b8dcb4057fad0ff075564da9ff5e011a2712
LSM <lsm4551@on.mjc.ac.kr>
2022-06-04 20:55:14 +0900

    areaInfo 데이터 구조 변경

--- src/common/areaInfo.js
+++ src/common/areaInfo.js
@@ -47,49 +47,52 @@
     }
     return [areaInfo[city].code, areaInfo[city].cityName, areaInfo[city][local]]
 }*/
-export const getAreaInfo = {
+export const areaInfo = {
         seoul: {
+            cityId: 'seoul',
             cityName: "서울특별시",
             regId: "11B10101",
-            gangnam: {
-                stationName: "강남구",
-                nx: "61",
-                ny: "126",
-                areaNo: "1168000000",
-                lat: 37.514575,
-                lng: 127.0495555
-            },
-            gangdong: {
-                stationName: "강동구",
-                nx: "62",
-                ny: "126",
-                areaNo: "1174000000",
-                lat: 37.52736667,
-                lng: 127.1258639
-            },
-            gangbuk: {
-                stationName: "강북구",
-                nx: "61",
-                ny: "128",
-                areaNo: "1130500000",
-                lat: 37.63695556,
-                lng: 127.0277194
-            },
-            gangseo: {
-                stationName: "강서구",
-                nx: "58",
-                ny: "126",
-                areaNo: "1150000000",
-                lat: 37.54815556,
-                lng: 126.851675
-            },
-            someArea: {
-                stationName: "",
-                nx: "",
-                ny: "",
-                areaNo: "",
-                lat: 2,
-                lng: 1
-            }
+            stations: [
+                {
+                    stationName: "강남구",
+                    nx: "61",
+                    ny: "126",
+                    areaNo: "1168000000",
+                    lat: 37.514575,
+                    lng: 127.0495555
+                },
+                {
+                    stationName: "강동구",
+                    nx: "62",
+                    ny: "126",
+                    areaNo: "1174000000",
+                    lat: 37.52736667,
+                    lng: 127.1258639
+                },
+                {
+                    stationName: "강북구",
+                    nx: "61",
+                    ny: "128",
+                    areaNo: "1130500000",
+                    lat: 37.63695556,
+                    lng: 127.0277194
+                },
+                {
+                    stationName: "강서구",
+                    nx: "58",
+                    ny: "126",
+                    areaNo: "1150000000",
+                    lat: 37.54815556,
+                    lng: 126.851675
+                }/*,
+                {
+                    stationName: "",
+                    nx: "",
+                    ny: "",
+                    areaNo: "",
+                    lat: 2,
+                    lng: 1
+                }*/
+            ]
         }
 }
