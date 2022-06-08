b2ee43684178e078777c4a133ad97d454ab4d246
rkdmf7220 <rkdmf7220@gmail.com>
2022-06-08 18:42:04 +0900

    vue devtools 실행 코드 작성 및 bootstrap 위치 app.vue로 이동

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import { BootstrapVue } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.config.devtools = true;
+
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue);
