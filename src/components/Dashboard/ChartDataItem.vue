fb78873cce46a0bafd3b15d715572cfbbb1a2afc
rkdmf7220 <rkdmf7220@gmail.com>
2022-06-26 21:31:03 +0900

    날씨 아이콘 변수에 낮/밤 추가

--- src/components/Dashboard/ChartDataItem.vue
+++ src/components/Dashboard/ChartDataItem.vue
@@ -12,6 +12,7 @@
 
 <script>
 import weatherSvgIcon from "../../../public/img/weatherSvgIcon";
+import moment from "moment";
 export default {
   name: "ChartDataItem",
   props: {
@@ -23,6 +24,33 @@ export default {
     },
     currentHour() {
       return this.itemData.hour ? this.itemData.hour + '시' : ''
+    },
+    isDaytime() {
+      let currentTime = this.itemData.hour + "00";
+      let checkTime
+      let dayTime
+      switch (this.itemData.day) {
+        case "today":
+          checkTime = moment(currentTime).isBetween(moment(this.$store.state.sunriseSunsetList[0].sunrise, 'h:mm A'), moment(this.$store.state.sunriseSunsetList[0].sunset, 'h:mm A'))
+              break;
+        case "tomorrow":
+          checkTime = moment(currentTime).isBetween(moment(this.$store.state.sunriseSunsetList[1].sunrise, 'h:mm A'), moment(this.$store.state.sunriseSunsetList[1].sunset, 'h:mm A'))
+              break;
+        case "afterTomorrow":
+          checkTime = moment(currentTime).isBetween(moment(this.$store.state.sunriseSunsetList[2].sunrise, 'h:mm A'), moment(this.$store.state.sunriseSunsetList[2].sunset, 'h:mm A'))
+              break;
+      }
+      switch (checkTime) {
+        case true:
+          dayTime = "0"
+              break;
+        case false:
+          dayTime = "1"
+              break;
+          default:
+            dayTime = "0"
+      }
+      return dayTime
     }
   },
   data () {
@@ -43,8 +71,8 @@ export default {
 
       .weather-icon-wrap{
         flex-grow: 0;
-        width: 16px;
-        height: 16px;
+        width: 24px;
+        height: 24px;
 
         .weather-icon{
           width: 100%;
