b18874e9efd209641b61ee19422c0272c3340012
rkdmf7220 <rkdmf7220@gmail.com>
2022-12-14 22:44:55 +0900

    vue devtools 미사용 처리

--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import store from './store'
 import router from './router'
 import { BootstrapVue } from 'bootstrap-vue'
 
-Vue.config.devtools = true;
-
-
 Vue.config.productionTip = false
 Vue.use(BootstrapVue);
 
